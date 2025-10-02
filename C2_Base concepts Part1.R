ae8705990ccadc43641ff67527055b95b029d6c7
Nagendra Sudanapalli <nsp.sudanapalli@gmail.com>
2025-10-02 16:31:41 +0530

    Created new files

---
+++ C2_Base concepts Part1.R
@@ -0,0 +1,38 @@
+#------------------------------------------------------------------------------#
+# Basic concepts - Part1                                                    ####
+#------------------------------------------------------------------------------#
+
+# 1.create a sample dataset
+# 2.copy a an existing dataset
+# 3.subset observations
+# 4.subset variables
+# 5.subset observations and variables
+# 6.renaming variables
+# 7.adding a new variables
+# 8.appending new datasets
+# 9.merging new datasets
+# 10.merging new datasets - select unmatched records
+# 11.Create a cartesian product
+# 12.sorting observatrions
+# 13.fetching frequenies
+# 14.descriptive for numeric variables
+# 15.first dot concept equilent to R
+# 16.Last dot concept - subset last reocrd within a subgroup
+# 17.first dot and last dot concept - identify the groups which has only one reocrd
+# 18.handling duplicates
+# 19.transpose the data - long to wide
+# 20.transpose the data - wide to long
+# 21.arrays - repeat the same logic / action on muliple variables
+
+
+
+
+
+#------------------------------------------------------------------------------#
+# 1.create a sample dataset                                                 ####
+#------------------------------------------------------------------------------#
+
+
+
+
+
